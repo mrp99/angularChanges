062f5738f34273fa46775be8c1081e3f053c620e
Marcelrp <marcel.pie@hotmail.com>
2024-07-21 21:15:40 -0300

    standalone components, step 04 - finally.

--- src/app/products/products.routes.ts
+++ src/app/products/products.routes.ts
@@ -1,16 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 
-const routes: Routes = [
+export const PRODUCT_ROUTES: Routes = [
   { path: '', component: ProductsListComponent },
   { path: 'new', component: ProductFormComponent }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class ProductsRoutingModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideNoopAnimations()
-    ]
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNoopAnimations(),
+    provideRouter(APP_ROUTES)
+  ]
 })
   .catch(err => console.error(err));
